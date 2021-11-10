50c3d458c2b07fa8a8ccd6dbbe9dc2ad6d68fd51
Dryele coelho <90875948+dryelecoelho@users.noreply.github.com>
2021-11-09 21:59:08 -0400

    Add files via upload

---
+++ fichaDeCadastro.py
@@ -0,0 +1,11 @@
+#String nome
+print('Jorge da silva', type('Jorge da silva'))
+
+#Int: idade
+print(32, type(32))
+
+#float: Altura
+print(1.75, type(1.75))
+
+#bool: Idade permitida
+print(17 < 18, type(17 < 18))
