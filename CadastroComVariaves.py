7e83478d6f15475a012105376cfadd0e52c1a552
Dryele coelho <90875948+dryelecoelho@users.noreply.github.com>
2021-11-11 22:02:09 -0400

    Delete CadastroComVariaves.py

--- CadastroComVariaves.py
+++
@@ -1,8 +0,0 @@
-nome = 'Afonso Luiz'
-idade = 22 # int
-altura = 1.70 # float
-e_maior = idade > 18 # bool
-peso = 78
-imc = peso / (altura ** 2)
-
-print(nome, 'tem', idade, 'anos de idade e seu imc é', imc)
