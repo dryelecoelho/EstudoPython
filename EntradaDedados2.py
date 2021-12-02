d6fa5af13f7d64d2f3fd2c9baeef37c9001f9720
Dryele coelho <90875948+dryelecoelho@users.noreply.github.com>
2021-12-02 14:35:36 -0300

    Add files via upload

---
+++ EntradaDedados2.py
@@ -0,0 +1,10 @@
+# Entrada de dados
+
+nome = input('Qual seu nome? ')
+idade = input('Qual sua idade? ')
+
+ano_nascimento = 2021-int(idade)
+
+print()
+print(f'{nome} tem {idade} anos.')
+print(f'{nome} nasceu em {ano_nascimento}.')
