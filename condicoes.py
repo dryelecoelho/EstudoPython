53d4804c317d0c698c97e12178688f097d243c22
Dryele coelho <90875948+dryelecoelho@users.noreply.github.com>
2021-12-02 14:56:06 -0300

    Add files via upload

---
+++ condicoes.py
@@ -0,0 +1,10 @@
+# Condicoes IF, ELIF E ELSE
+
+if False:
+    print("verdadeiro.")
+elif True:
+    print("agora é verdadeiro")
+elif False:
+    print("mais uma verdadeira")
+else:
+    print('nao é verdadeira')
